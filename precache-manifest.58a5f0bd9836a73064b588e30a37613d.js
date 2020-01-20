100e6e8fcb65de0d78b38dfa81d4ae8b5f8f10d1
valeriia.klimova <valeriia.klimova@x2sy.com>
2020-01-20 10:13:06 +0200

    Updates

---
+++ precache-manifest.58a5f0bd9836a73064b588e30a37613d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbe3615a0848deea469c4533b2f9063",
+    "url": "/starnavi/index.html"
+  },
+  {
+    "revision": "f981b0fe57d8730d30c7",
+    "url": "/starnavi/static/css/main.073ba4f0.chunk.css"
+  },
+  {
+    "revision": "a1e3caa672c2ed4d7cce",
+    "url": "/starnavi/static/js/2.c662e7f3.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/starnavi/static/js/2.c662e7f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f981b0fe57d8730d30c7",
+    "url": "/starnavi/static/js/main.ea6c5a82.chunk.js"
+  },
+  {
+    "revision": "611ff3160f0924651d72",
+    "url": "/starnavi/static/js/runtime-main.83b2b7e9.js"
+  }
+]);
